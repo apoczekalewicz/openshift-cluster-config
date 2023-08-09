26ffde013cd13793e835dde427ce45f685d4ff9a
Artur <artur@poczekalewicz.net>
2023-08-09 22:07:52 +0200

    commit

--- install/start.sh
+++ install/start.sh
@@ -1,12 +1,15 @@
 oc apply --wait=true -f gitops/gitops-operator.yaml
-echo "Approval install-plan"
-PLAN=$(oc get installplan -A | grep openshift-gitops-operator | awk '{print $2}')
-oc patch installplan $PLAN --namespace openshift-operators --type merge --patch '{"spec":{"approved":true}}'
+#echo "Approval install-plan"
+#PLAN=$(oc get installplan -A | grep openshift-gitops-operator | awk '{print $2}')
+#oc patch installplan $PLAN --namespace openshift-operators --type merge --patch '{"spec":{"approved":true}}'
 
 sleep 5 
 
 echo "Add cluster-admin to the openshift-gitops-argocd-application-controller"
 oc adm policy add-cluster-role-to-user cluster-admin -z openshift-gitops-argocd-application-controller -n openshift-gitops
+
+sleep 5
+
 echo ArgoCD route:
 oc get -n openshift-gitops route
 echo Admin password:
